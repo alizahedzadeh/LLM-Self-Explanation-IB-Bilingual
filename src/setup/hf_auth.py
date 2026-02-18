ed752063be2aa366d024dfd807061ec6b64c5e5d
Ali Zahedzadeh <alizahedzadeh7@gmail.com>
2026-02-18 10:53:24 +0000

    Fix huggingface_login

--- src/setup/hf_auth.py
+++ src/setup/hf_auth.py
@@ -1,4 +1,12 @@
+import os
+from dotenv import load_dotenv
 from huggingface_hub import login
 
 def hf_login():
-    login("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")
+    load_dotenv()  # load .env file
+    token = os.getenv("HF_TOKEN")
+
+    if token is None:
+        raise ValueError("HF_TOKEN not found in environment variables")
+
+    login(token)
